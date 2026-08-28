e93bdc7248837405b93323c3c1dc05c61fcf8e25
NextNeuro <kynetic992@gmail.com>
2026-08-28 11:02:09 +0800

    fix: correct some texts,add site url to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://xh.asxz.one',
 	integrations: [
 		starlight({
 			title: 'Xuehai Docs',
